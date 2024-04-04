a5ccddfe1ff16787e1803f104b336bb128f6223d
ferMadrid92 <fer.madridb@gmail.com>
2024-04-04 12:54:52 -0500

    implementación de use reducer

--- src/components/OrderContent.tsx
+++ src/components/OrderContent.tsx
@@ -1,12 +1,14 @@
+import { Dispatch } from "react"
 import { formatCurrency } from "../helpers"
-import { MenuItem, OrderItem } from "../types"
+import { OrderItem } from "../types"
+import { OrderActions } from "../reducers/order-reducer"
 
 type OrderContentProps = {
     order: OrderItem[],
-    removeItem: (id: MenuItem['id']) => void
+    dispatch: Dispatch<OrderActions>
 }
 
-export default function OrderContent({order, removeItem} : OrderContentProps) {
+export default function OrderContent({order, dispatch} : OrderContentProps) {
   return (
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
@@ -26,7 +28,7 @@ export default function OrderContent({order, removeItem} : OrderContentProps) {
                         </div>
                         <button
                             className="bg-red-600 h-8 w-8 rounded-full text-white"
-                            onClick={() => removeItem(item.id)}
+                            onClick={() => dispatch({type: 'remove-item', payload: {id: item.id}})}
                         >X</button>
                     </div>
                 ))}
